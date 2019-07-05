50632c59db3ff273f9de354695e92d0ee83c3491
ForG7 <zhao13641292642@163.com>
2019-07-05 15:22:14 +0800

    add two files

---
+++ components/alert.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h3></h3>
+        <p></p>
+        <div><a href=""></a><a href=""></a></div>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: "alert"
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ components/tab.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <p><span></span><span></span><span></span></p>
+        <div></div>
+        <div></div>
+        <div></div>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: "tab"
+  }
+</script>
+
+<style scoped>
+
+</style>
